bacdec5fd779e30d3e875e4748e906577ff6d49f
Xiao Zhang <xiaozhang446@live.ca>
2025-06-09 15:52:56 -0500

    Fixed formatting issues with Astra theme

--- build/overlay/index.asset.php
+++ build/overlay/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ae9ee59df49448ed9f5b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e7c1482858504ff8acd9');
